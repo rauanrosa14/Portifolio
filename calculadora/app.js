cc57ee136fb6beb7049618d38ccfc9f7042ec435
rauanrosa14 <88544246+rauanrosa14@users.noreply.github.com>
2022-01-21 21:12:42 -0300

    ADD Portifoli web site

---
+++ calculadora/app.js
@@ -0,0 +1,29 @@
+nums = []
+result = 0;
+
+function add(num1,num2){
+    result  = num1+num2;
+    return(num1+num2);
+}
+
+function sub(num1,num2){
+    result  = num1-num2
+    return(num1-num2);
+}
+
+function div(num1,num2){
+    result  = num1/num2;
+    return(num1/num2);
+}
+
+function  mul(num1,num2){
+    result  = num1*num2
+    return(num1*num2);
+}
+
+function addNum(num){
+    document.querySelector("tela-res") =  `<h4>`+num +`</h4>`;
+}
+
+
+console.log();
